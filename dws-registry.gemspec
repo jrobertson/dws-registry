c255a0c0e5768371fc00928f6c5c7b235f8ae7ee
James Robertson <james@r0bertson.co.uk>
2015-03-03 21:48:17 +0000

    feature: the registry now attempts to identify the data type when using the methods import(), set_key() or get_key().

--- dws-registry.gemspec
+++ dws-registry.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'dws-registry'
-  s.version = '0.1.3'
+  s.version = '0.2.0'
   s.summary = 'dws-registry'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('xml-registry') 
+  s.add_runtime_dependency('xml-registry', '~> 0.3', '>=0.3.1') 
   s.signing_key = '../privatekeys/dws-registry.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/dws-registry'
+  s.required_ruby_version = '>= 2.1.2'
 end
--- lib/dws-registry.rb
+++ lib/dws-registry.rb
@@ -2,14 +2,16 @@
 
 # file: dws-registry.rb
 
-
+require 'time'
 require 'xml-registry'
 
 class DWSRegistry < XMLRegistry
   
-  def initialize(filename=nil)
+  def initialize(filename='registry.xml', autosave: true)
     
     super()
+    
+    @autosave = autosave    
     
     if filename then
 
@@ -23,15 +25,56 @@ class DWSRegistry < XMLRegistry
 
     end
   end
+  
+  def get_key(path)
+    
+    e = super path
+    c = e.attributes[:class]
+    s = e.text
+    
+    return e if s.nil?    
+    return s unless c
+          
+    h = {
+      string: ->(x) {x},
+      boolean: ->(x){ 
+        case x
+        when 'true' then true
+        when 'false' then false
+        else x
+        end
+      },
+      number: ->(x){  x[/^[0-9]+$/] ? x.to_i : x.to_f },
+      time:   ->(x) {Time.parse x}
+    }
+                            
+    h[c.to_sym].call s
+    
+  end
 
   def set_key(path, value)
-    super(path, value)
-    save()
+    
+    value, type = case value.class.to_s.downcase.to_sym
+    when :string     then value
+    when :time       then ["#%s#" % value.to_s, :time]
+    when :fixnum     then [value.to_s, :number]
+    when :float      then [value.to_s, :number]      
+    when :falseclass then [value.to_s, :boolean]
+    when :trueclass  then [value.to_s, :boolean]
+    end
+    
+    e = super(path, value)    
+
+    type = find_class value unless type
+    e.attributes[:class] = type if type
+    
+    save() if @autosave
+    e
   end
 
   def delete_key(path)
     super(path)
-    save()
+    save() if @autosave
   end
 
   def save(filename=nil)
@@ -41,11 +84,28 @@ class DWSRegistry < XMLRegistry
 
   def import(s)      
     super(s)
-    save()
+    save() if @autosave
   end
   
   def refresh()
     load_xml(@filename)    
+  end
+  
+  private
+  
+  def add_value(key, value)
+    e = @doc.root.element(key)
+    e.text = value
+    return e
+  end
+  
+  def find_class(v)
+
+    if v.to_i.to_s.length == v.length then :number
+    elsif v.to_f.to_s.length == v.length then :number
+    elsif v.downcase[/^(?:true|false|on|off|yes|no)$/] then :boolean
+    elsif v[/^\#.*\#$/] then :time
+    end
   end  
 
 end
